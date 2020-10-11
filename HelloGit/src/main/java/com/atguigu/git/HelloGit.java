6eb7f8a39f0335575bb6759302de063a30963667
1514015811@qq.com <1514015811@qq.com>
2020-10-11 16:46:29 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+    public String sayHello(String name){
+        return "Hello"+name;
+    }
+}
